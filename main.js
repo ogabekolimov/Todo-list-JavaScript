493da62a82d4f1b3cf1421ee1fa7bd1e48d38668
ramziddin_olimov <ramizmafiya@gmail.com>
2021-04-04 10:52:17 +0500

    JavaScript started and continued

---
+++ main.js
@@ -0,0 +1,40 @@
+const inputElement = document.querySelector('input');
+const buttonElement = document.querySelector('.addTask > button');
+
+buttonElement.addEventListener('click', addList);
+inputElement.addEventListener('keyup', (e)=>{
+    (e.keycode === 13 ? addList(e) : null);
+})
+
+function addList(e){
+    const notCompleted = document.querySelector('.notCompleted');
+    const Completed = document.querySelector('.Completed');
+
+    const newLi = document.createElement('li')
+    const checkBtn = document.createElement('button')
+    const delBtn = document.createElement('button')
+
+    checkBtn.innerHTML = '<i class="fa fa-check"></i>';
+    delBtn.innerHTML = '<i class="fa fa-trash"></i>';
+
+    if(inputElement.value !==''){
+        newLi.textContent = inputElement.value;
+        inputElement.value = '';
+        notCompleted.appendChild(newLi);
+        newLi.appendChild(checkBtn);
+        newLi.appendChild(delBtn);
+    }
+
+    checkBtn.addEventListener('click', function(){
+        const parent = this.parentNode;
+        parent.remove();
+        Completed.appendChild(parent);
+        checkBtn.style.display = 'none';
+    });
+
+    delBtn.addEventListener('click', function(){
+        const parent = this.parentNode;
+        parent.remove();
+        
+    });
+}
